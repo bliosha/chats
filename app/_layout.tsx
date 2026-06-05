5f7c353f9ac21b7b22d6b739c84ed8bb09acf783
liofikkk <l4212550@gmail.com>
2026-06-05 15:18:17 +0300

    added husky

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,13 +8,13 @@ export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <KeyboardProvider>
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-        <Stack screenOptions={{ contentStyle: { backgroundColor: 'white' } }}>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="contacts" options={{ headerShown: false }} />
-          <Stack.Screen name="chat/[id]" options={{ headerShown: false }} />
-        </Stack>
-      </SafeAreaView>
+        <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
+          <Stack screenOptions={{ contentStyle: { backgroundColor: 'white' } }}>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen name="contacts" options={{ headerShown: false }} />
+            <Stack.Screen name="chat/[id]" options={{ headerShown: false }} />
+          </Stack>
+        </SafeAreaView>
       </KeyboardProvider>
     </SafeAreaProvider>
   )
